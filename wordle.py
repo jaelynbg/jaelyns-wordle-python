7599f908caa7a082d40e7a6e78ec5d0ee057fd06
Jaelyn <jaelynbgarrard@gmail.com>
2026-08-24 10:32:13 -0700

    creating different themes light mode dark mode blue mode pink mode etc

--- wordle.py
+++ wordle.py
@@ -7,6 +7,48 @@ from string import ascii_letters
 NUM_LETTERS = 5
 NUM_GUESSES = 6
 
+THEMES = {
+    "Light": {
+        "background": "#FFFFFF",
+        "text": "#000000",
+        "border": "#D3D6DA",
+        "keyboard": "#D3D6DA",
+        "correct": "#6AAA64",
+        "misplaced": "#C9B458",
+        "wrong": "#787C7E"
+    },
+
+    "Dark": {
+        "background": "#121213",
+        "text": "#FFFFFF",
+        "border": "#3A3A3C",
+        "keyboard": "#818384",
+        "correct": "#538D4E",
+        "misplaced": "#B59F3B",
+        "wrong": "#3A3A3C"
+    },
+
+    "Blue": {
+        "background": "#EAF4FF",
+        "text": "#12304A",
+        "border": "#9CC7E8",
+        "keyboard": "#A9D6F5",
+        "correct": "#2E86AB",
+        "misplaced": "#F2C14E",
+        "wrong": "#6C757D"
+    },
+
+    "Pink": {
+        "background": "#FFF0F6",
+        "text": "#5A1833",
+        "border": "#F2B5D4",
+        "keyboard": "#F4C2D7",
+        "correct": "#D63384",
+        "misplaced": "#E0A458",
+        "wrong": "#8A6878"
+    }
+}
+
 
 def create_keyboard(root, guess_entry):
     keyboard_frame = tk.Frame(root)
